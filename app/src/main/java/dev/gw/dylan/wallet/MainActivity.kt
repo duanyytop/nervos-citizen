d65be2128b38cef80f4e62426b718572511ed36d
duanyytop <duanyytop@gmail.com>
2020-09-16 16:23:21 +0800

    chore: Update theme and style

--- app/src/main/java/dev/gw/dylan/wallet/MainActivity.kt
+++ app/src/main/java/dev/gw/dylan/wallet/MainActivity.kt
@@ -29,25 +29,9 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
-
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        // Check if we got documentdata and set the documentData attribute
+        // Check if we got documentData and set the documentData attribute
         if (requestCode == GET_DOC_INFO && resultCode == RESULT_OK) {
             val documentData = data?.extras!![DocumentData.Identifier] as DocumentData?
             val intent = Intent(this, PassportConActivity::class.java)
